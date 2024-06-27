bdd6819526b551ba3691c2ed9916f0063b5d2573
Mohammed Shanu <mhdshanu101@gmail.com>
2024-06-27 19:59:49 +0300

    Complete version 1

---
+++ index.js
@@ -0,0 +1,60 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "mhdshanu@123",
+  port: 5432,
+});
+db.connect();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+app.get("/", async(req, res) => {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+  console.log(result.rows);
+  const newTitle = result.rows;
+  
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: newTitle,
+  });
+});
+
+app.post("/add", async(req, res) => { //When clicked on add the typed name will be added to the array. So when redirected, that too appears in homepage.
+  const item = req.body.newItem;  //newItem is the name of the input field
+  items.push({ title: item });
+  const result = await db.query("INSERT INTO items(title) VALUES($1)", [item]);
+  console.log(result.rows);
+  res.redirect("/");
+});
+
+app.post("/edit", async(req, res) => {
+  const editClick = req.body.updatedItemId
+  const editTitle = req.body.updatedItemTitle;
+  await db.query("UPDATE items SET title = $1 WHERE id = $2", [editTitle,editClick]);
+  res.redirect("/");
+});
+
+app.post("/delete", async(req, res) => {
+  const deleteCheck = req.body.deleteItemId;
+  await db.query("DELETE FROM items WHERE id = $1", [deleteCheck]);
+  res.redirect("/");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
